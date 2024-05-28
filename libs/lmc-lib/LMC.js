257e6af370d28d514e07699650bcab74f97efa47
donulo <donulo45@gmail.com>
2024-05-29 02:34:02 +0300

    tai

--- libs/lmc-lib/LMC.js
+++ libs/lmc-lib/LMC.js
@@ -1,22 +1,24 @@
-import Compiler from "./Compiler.js";
-import Instruction from "./Instruction.js";
+import Compiler from './Compiler.js';
+import Instruction from './Instruction.js';
+
 export default class LMC {
-    controller = {
-        input: "",
-        output: "",
-        program_counter: 0,
-        accumulator: 0,
-        memory: []
-    };
-    compiler;
+  controller = {
+    input: '',
+    output: '',
+    program_counter: 0,
+    accumulator: 0,
+    memory: [100].fill('000')
+  };
+  compiler;
 
-    constructor(text) {
-        this.compiler = new Compiler(text);
-        this.controller.memory = this.compiler.machine_code;
-    }
+  constructor(text) {
+    this.compiler = new Compiler(text);
+    this.controller.memory = this.compiler.machine_code;
+  }
 
-    async step() {
-        var instruction = new Instruction(this.controller.memory, this.controller.program_counter);
-        return await instruction.execute(this.controller);
-    }
+  async step() {
+    var instruction = new Instruction(
+        this.controller.memory, this.controller.program_counter);
+    return await instruction.execute(this.controller);
+  }
 }
--- script.js
+++ script.js
@@ -1,51 +1,85 @@
-import LMC from "./libs/lmc-lib/LMC.js";
+import LMC from './libs/lmc-lib/LMC.js';
 
-var memory = document.getElementById("memory");
-for (var i = 0; i < 10; i++) {
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+var program_counter = document.getElementById('program_counter');
+var accumulator = document.getElementById('accumulator');
+var memory = document.getElementById('memory');
+var lmc = new LMC('');
+
+initTable();
+drawing();
+
+function initTable() {
+  for (var i = 0; i < 10; i++) {
     var row = memory.insertRow();
     for (var j = 0; j < 10; j++) {
-        var cell = row.insertCell();
-        cell.innerHTML = "000";
+      row.insertCell();
     }
+  }
 }
-var input = document.getElementById("input");
-var output = document.getElementById("output");
-var program_counter = document.getElementById("program_counter");
-var accumulator = document.getElementById("accumulator");
-
-var lmc = new LMC("");
 
 function drawing() {
-    var trs = (memory.children)[0].children;
-    for (var i = 0; i < trs.length; i++) {
-        var tds = trs[i].children;
-        for (var j = 0; j < tds.length; j++) {
-            tds[j].innerHTML = lmc.controller.memory[i * trs.length + j];
-        }
+  var trs = (memory.children)[0].children;
+  for (var i = 0; i < trs.length; i++) {
+    var tds = trs[i].children;
+    for (var j = 0; j < tds.length; j++) {
+      tds[j].innerHTML = lmc.controller.memory[i * trs.length + j];
+      var label = document.createElement('label');
+      label.setAttribute('class', 'labelCell');
+      label.innerHTML = 10 * i + j;
+      tds[j].prepend(label);
     }
-    input.value = lmc.controller.input;
-    output.value = lmc.controller.output;
-    program_counter.value = lmc.controller.program_counter;
-    accumulator.value = lmc.controller.accumulator;
+  }
+  input.value = lmc.controller.input;
+  output.value = lmc.controller.output;
+  program_counter.value = lmc.controller.program_counter;
+  accumulator.value = lmc.controller.accumulator;
 }
 
 function compile() {
-    var text = document.getElementById("inputArea").value;
-    lmc = new LMC(text);
-    document.getElementById("outputArea").value = lmc.compiler.intermediate_code;
-    drawing();
+  var text = document.getElementById('inputArea').value;
+  lmc = new LMC(text);
+  document.getElementById('outputArea').value = lmc.compiler.intermediate_code;
+  clearBorder();
+  drawing();
 }
-document.querySelector("#compile").addEventListener('click', compile);
+document.querySelector('#compile').addEventListener('click', compile);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function run() {
-    while (await lmc.step() != "EXIT") {
-        drawing();
-    }
+  do {
+    clearBorder();
+    currentBorder(lmc.controller.program_counter);
+    drawing();
+    await sleep(1000);
+  } while (await lmc.step() != 'EXIT')
 }
-document.querySelector("#run").addEventListener('click', run);
+document.querySelector('#run').addEventListener('click', run);
 
 async function step() {
-    await lmc.step();
-    drawing();
+  clearBorder();
+  currentBorder(lmc.controller.program_counter);
+  await lmc.step();
+  drawing();
 }
-document.querySelector("#step").addEventListener('click', step);
+document.querySelector('#step').addEventListener('click', step);
+
+function clearBorder() {
+  var border = '1px solid #ccc';
+  var trs = (memory.children)[0].children;
+  for (var i = 0; i < trs.length; i++) {
+    var tds = trs[i].children;
+    for (var j = 0; j < tds.length; j++) {
+      tds[j].style.border = border;
+    }
+  }
+}
+function currentBorder(program_counter) {
+  var trs = (memory.children)[0].children
+  var tds = trs[parseInt(program_counter / 10)].children;
+  tds[program_counter % 10].style.border = '1px solid green';
+}
